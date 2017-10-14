6751d56b24810c985c26ff9f8c8e696be6641ec6
Sohel Shekha <shekha.sohel@gmail.com>
2017-10-14 13:57:16 +0800

    Getting Started with Xamarin.Mac

---
+++ HelloMacNetConf/ViewController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace HelloMacNetConf
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+            HelloButton.Activated += (sender, e) => 
+            {
+                Console.WriteLine("Hello");
+            };
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
